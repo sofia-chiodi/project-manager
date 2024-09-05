51d914e7337193d528b97e328b7ab1e9454e6f6c
sofia-chiodi <chiodi.sb@gmail.com>
2024-09-04 21:22:57 -0300

    Adding Button and NoProjectSelected components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from 'react';
+
 import Sidebar from './components/Sidebar';
-import Content from './components/Content';
+import NoProjectSelected from './components/NoProjectSelected';
 import NewProject from './components/NewProject';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined, // Nothing
+    projects: [],
+  });
+
+  function handleAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null, // Adding a new project
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onAddProject={handleAddProject} />;
+  }
+
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <Sidebar />
-      <NewProject />
+      <Sidebar onAddProject={handleAddProject} />
+      {content}
     </main>
   );
 }
