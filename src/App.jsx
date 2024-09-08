e20d0a521bce1db58ca9eda5a15588772e57b802
sofia-chiodi <chiodi.sb@gmail.com>
2024-09-08 10:36:33 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
       return {
         ...prevState,
-        selectedProjectId: projectId,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
